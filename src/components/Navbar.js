65b1da9da1659512cee2546563da9a7e86826d8a
DaianaArena <84255114+DaianaArena@users.noreply.github.com>
2022-02-14 05:02:14 -0300

    adding details and styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,7 @@ export default function Navbar() {
     return (
         <nav className="navbar">
             <img src={logo} alt="logo"  className="logo" />
+            <p>Coded by <a href="https://github.com/DaianaArena">Daiana Arena</a></p>
         </nav>
     )
 }
